bdb629d44fc61d87c27a6dce1ab04a5758c575ac
Joao Ormeño <joao23.1996@gmail.com>
2022-06-13 22:09:32 -0400

    modificando escritorio

--- public/js/escritorio.js
+++ public/js/escritorio.js
@@ -1,3 +1,40 @@
+//referencias html
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector('button');
+//const lblEscritorio = document.querySelector('h1');
+
+const searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('escritorio')) {
+    window.location = 'index.html'
+    throw new Error('El escritorio es obligatorio');
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText = escritorio;
+
+const socket = io();
 
 
-console.log('Escritorio HTML');
+
+socket.on('connect', () => {
+    btnAtender.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnAtender.disabled = true;
+});
+
+socket.on('ultimo-ticket', (ultimo) => {
+    //lblNuevoTicket.innerText = 'Ticket ' + ultimo;
+})
+
+
+btnAtender.addEventListener( 'click', () => {
+
+    
+    // socket.emit( 'siguiente-ticket', null, ( ticket ) => {
+    //     console.log('Desde el server', ticket );
+    // });
+
+});
